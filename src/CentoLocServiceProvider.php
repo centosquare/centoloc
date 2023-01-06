d4066039bc4a34ec1e8688a09e6e64c0f6ea50de
Muhammad Waqas <mwaqasiu@gmail.com>
2023-01-06 12:13:09 +0500

    initial setup for package

---
+++ src/CentoLocServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+namespace CentoSquare\CentoLoc;
+
+use Illuminate\Support\ServiceProvider;
+
+class CentoLocServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any package services.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        //
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        //
+    }
+}
